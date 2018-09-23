46ef83c49b4d2f139f3bdc787d0bdfe55375578f
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-09-23 17:15:23 +0100

    reload source code

--- app/Index.tsx
+++ app/Index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Hello } from './components/Hello';
-console.log('test');
+
 ReactDOM.render(<Hello compiler="TypeScript" framework="React" />, document.getElementById('example'));
