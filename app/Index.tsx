49b8e97c91c357f4a8f30c48bfca6c11f34d586f
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-09-24 16:24:15 +0100

    chore(TSLint): change max length to 100 (#4)

--- app/Index.tsx
+++ app/Index.tsx
@@ -2,4 +2,7 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Hello } from './components/Hello';
 
-ReactDom.render(<Hello compiler="TypeScript" framework="React" />, document.getElementById('example'));
+ReactDom.render(
+  <Hello compiler="TypeScript" framework="React" />,
+  document.getElementById('example')
+);
