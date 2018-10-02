fd60f90a48d41814b80ddcc9c6d8ecdf93db21ee
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-10-02 21:23:43 +0100

    finish dist command

--- app/Index.tsx
+++ app/Index.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Hello } from './components/Hello';
 
-ReactDom.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById('example')
-);
+const appElement: HTMLElement = document.getElementById('app');
+
+if (appElement) {
+  ReactDom.render(<Hello compiler="TypeScript" framework="React" />, appElement);
+}
