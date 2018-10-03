1dd49e9f7130d01a5758a39725ed27e3e69f470f
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-10-03 09:30:23 +0100

    fix: fix auto-reloading of the app

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron');
 
+// Enable hot reloading when running in development mode
+require('electron-reload')(__dirname);
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
