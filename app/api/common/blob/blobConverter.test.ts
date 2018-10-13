8f91058ccd626bf63c7d0ebc56ac47d4a6f43473
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-10-13 17:30:00 +0100

    feat: decode Base64 URL data

--- app/api/common/blob/blobConverter.test.ts
+++ app/api/common/blob/blobConverter.test.ts
@@ -31,4 +31,24 @@ describe('blobConverter class', () => {
       expect(actualText).toEqual(mockDataURL);
     });
   });
+
+  describe('decodeDataURL function', () => {
+    it('should decode data URL from Base 64', () => {
+      const encodedDataURL = 'data:text/plain;charset=utf-8;base64,SGVsbG8sIFdvcmxkIQ==';
+      const expectedDataURL = 'Hello, World!';
+
+      const actualDataURL = BlobConverter.decodeDataURL(encodedDataURL);
+
+      expect(actualDataURL).toBeDefined();
+      expect(actualDataURL).toEqual(expectedDataURL);
+    });
+
+    it('should throw error if contains corrupted Base64 data', () => {
+      const encodedDataURL = 'data:text/plain;charset=utf-8;base64,S%VsbG8sIFdvcmxkIQ==';
+
+      const actualData = () => BlobConverter.decodeDataURL(encodedDataURL);
+
+      expect(actualData).toThrowError('Incorrect Base64 encoding');
+    });
+  });
 });
