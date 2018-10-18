f591ba12d356c66f7d2156d460c3c1f10aa91733
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-10-18 21:09:36 +0100

    write unit tests

--- app/api/common/id3/id3Reader.test.ts
+++ app/api/common/id3/id3Reader.test.ts
@@ -1,5 +1,9 @@
+import BlobUtil from '../blob/blobUtil';
+
 describe('id3Reader', () => {
   describe('readID3 function', () => {
-    it('should create ID3 object from DataView', () => {});
+    it('should create ID3 object from DataView', () => {
+      const title: DataView = new DataView(BlobUtil.createArrayBuffer('Example Artist'), 3, 30);
+    });
   });
 });
