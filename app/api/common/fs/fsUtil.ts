c9271277cc02092ad7ec6c07cce9757de4b218c3
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-10-29 09:14:14 +0000

    fix linting issue

--- app/api/common/fs/fsUtil.ts
+++ app/api/common/fs/fsUtil.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 export default class FsUtil {
   public static readFile = async (path: fs.PathLike): Promise<Buffer> => {
-    return <Promise<Buffer>>new Promise((resolve, reject) => {
+    return new Promise<Buffer>((resolve, reject) => {
       fs.readFile(path, (err: any, data: Buffer) => {
         if (err) {
           reject(err);
