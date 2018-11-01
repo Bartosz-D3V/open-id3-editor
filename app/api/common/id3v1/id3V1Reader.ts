e82deb11dd352c3984bcf4f0dc15491aa912fa32
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-11-01 20:52:41 +0000

    restructure id3V1 reader

--- app/api/common/id3v1/id3V1Reader.ts
+++ app/api/common/id3v1/id3V1Reader.ts
@@ -1,11 +1,11 @@
-import ID3 from './id3';
+import ID3V1 from './id3V1';
 import BlobUtil from '../blob/blobUtil';
 
-export default class Id3Reader {
-  private static readonly MISSING_ID3 = 'Invalid MP3 file - ID3 tag is missing';
+export default class Id3V1Reader {
+  private static readonly MISSING_ID3 = 'Invalid MP3 file - ID3V1 tag is missing';
 
-  public static readID3(dataView: DataView): ID3 {
-    const offset: number = Id3Reader.getID3TagOffset(dataView);
+  public static readID3V1(dataView: DataView): ID3V1 {
+    const offset: number = Id3V1Reader.getID3V1TagOffset(dataView);
     const title: string = BlobUtil.dataViewToString(dataView, offset + 3, 30);
     const artist: string = BlobUtil.dataViewToString(dataView, offset + 33, 30);
     const album: string = BlobUtil.dataViewToString(dataView, offset + 63, 30);
@@ -14,18 +14,14 @@ export default class Id3Reader {
     const zeroByte: boolean = !!BlobUtil.dataViewToString(dataView, offset + 125, 1);
     const track: number = BlobUtil.dataViewToNum(dataView, offset + 126);
     const genre: number = BlobUtil.dataViewToNum(dataView, offset + 127);
-    return new ID3(title, artist, album, year, comment, zeroByte, track, genre);
+    return new ID3V1(title, artist, album, year, comment, zeroByte, track, genre);
   }
 
-  private static getID3TagOffset(dataView: DataView): number {
-    const startTag: string = BlobUtil.dataViewToString(dataView, 0, 3);
-    const endTag: string = BlobUtil.dataViewToString(dataView, dataView.byteLength - 128, 3);
-    if (startTag === 'TAG') {
-      return 0;
-    } else if (endTag === 'TAG') {
+  private static getID3V1TagOffset(dataView: DataView): number {
+    const tag: string = BlobUtil.dataViewToString(dataView, dataView.byteLength - 128, 3);
+    if (tag === 'TAG') {
       return dataView.byteLength - 128;
-    } else {
-      throw new Error(Id3Reader.MISSING_ID3);
     }
+    throw new Error(Id3V1Reader.MISSING_ID3);
   }
 }
