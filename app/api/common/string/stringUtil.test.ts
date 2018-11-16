ee6479c2f66cef43d11d31a176943c358dcc7225
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-11-16 11:52:55 +0000

    tests: improved test cases for stringUtil method

--- app/api/common/string/stringUtil.test.ts
+++ app/api/common/string/stringUtil.test.ts
@@ -3,10 +3,11 @@ import StringUtil from './stringUtil';
 describe('StringUtil class', () => {
   describe('getByteAt method', () => {
     it('should return byte from the given index', () => {
+      expect(StringUtil.getByteAt('A', 2)).toEqual(0);
       expect(StringUtil.getByteAt('1', 0)).toEqual(49);
-      expect(StringUtil.getByteAt('s', 0)).toEqual(115);
-      expect(StringUtil.getByteAt('a', 0)).toEqual(97);
-      expect(StringUtil.getByteAt('$', 0)).toEqual(36);
+      expect(StringUtil.getByteAt('Test', 2)).toEqual(115);
+      expect(StringUtil.getByteAt('alpha', 0)).toEqual(97);
+      expect(StringUtil.getByteAt('JqueryUses$CharHeavily', 10)).toEqual(36);
     });
   });
 });
