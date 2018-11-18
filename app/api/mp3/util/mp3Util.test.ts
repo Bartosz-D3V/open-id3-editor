9b24ad495aca6a3030649d066966fe519c885c4d
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-11-18 17:50:23 +0000

    feat: add function to remove id3 tags (#48)

--- app/api/mp3/util/mp3Util.test.ts
+++ app/api/mp3/util/mp3Util.test.ts
@@ -1,6 +1,7 @@
 import BufferUtil from '../../common/buffer/bufferUtil';
 import Mp3Util from './mp3Util';
 import BlobUtil from '../../common/blob/blobUtil';
+import FsUtil from '../../common/fs/fsUtil';
 
 describe('mp3Util', () => {
   describe('hasID3V1 function', () => {
@@ -35,6 +36,30 @@ describe('mp3Util', () => {
       const mockFile: DataView = new DataView(tag);
 
       expect(Mp3Util.hasID3V2(mockFile)).toBeFalsy();
+    });
+  });
+
+  describe('getMP3WithoutTags function', () => {
+    it('should return original dataView if it does not contain tags', () => {
+      const dataView: DataView = new DataView(new ArrayBuffer(200));
+
+      expect(Mp3Util.getMP3WithoutTags(dataView)).toEqual(dataView);
+    });
+
+    it('should return return dataView with removed tags', async () => {
+      const mockMp3: Buffer = await FsUtil.readFile(
+        `${__dirname}/../id3v2/mockID3Files/id3v2_001_basic.mp3`
+      );
+      const mockMp3NoTags: Buffer = await FsUtil.readFile(
+        `${__dirname}/../id3v2/mockID3Files/id3v2_001_no_tags.mp3`
+      );
+      const mockMp3View: DataView = new DataView(mockMp3.buffer);
+      const mockMp3NoTagsView: DataView = new DataView(mockMp3NoTags.buffer);
+      const strippedMockView = Mp3Util.getMP3WithoutTags(mockMp3View);
+
+      expect(mockMp3View).toEqual(mockMp3NoTagsView);
+      expect(strippedMockView.byteLength).toBeLessThan(128);
+      expect(BlobUtil.dataViewToString(strippedMockView, 0, 3)).not.toEqual('ID3');
     });
   });
 });
