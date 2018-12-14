ad8cb114cb765b830d3524fbf1cc75d84face48f
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-12-14 17:14:36 +0000

    fix issue with generic interface (#74)

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from '@components/App';
 import 'antd/dist/antd.css';
+import { App } from '@components/App';
 
 const appElement: HTMLElement = document.getElementById('app');
 
