719fd785d936aae99a47bde0df5372dbc10becc5
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-12-28 18:41:05 +0100

    feat: display-id3 but without genre (for now)

--- app/components/TagForm/V1-0/TagFormV1-0.tsx
+++ app/components/TagForm/V1-0/TagFormV1-0.tsx
@@ -5,6 +5,9 @@ import { genres } from '@api/id3v1/domain/genres';
 import Genre from '@api/id3v1/domain/genre';
 import Mp3Util from '@api/common/mp3/mp3Util';
 import BlobUtil from '@api/common/blob/blobUtil';
+import ID3V10 from '@api/id3v1/domain/id3V1-0';
+import Id3Reader from '@api/id3v1/id3Reader';
+import blobUtil from '@api/common/blob/blobUtil';
 
 const TextArea = Input.TextArea;
 
@@ -17,51 +20,59 @@ const twoInCol = {
 };
 
 export class TagFormV10 extends Component<ITagFormV10Props> {
+  private state = {
+    id3: ID3V10,
+  };
+
   public async componentDidMount(): Promise<void> {
     const { selectedFile } = this.props;
     const dataView: DataView = await BlobUtil.blobToDataView(selectedFile.originFileObj);
     if (Mp3Util.hasID3V1(dataView)) {
+      const id3: ID3V10 = Id3Reader.readID3V10(dataView);
+      this.setState({ id3 });
     }
   }
 
   public render(): JSX.Element {
+    const { id3 }: { id3: ID3V10 } = this.state;
+
     return (
       <Form>
         <Row gutter={5} justify="space-around">
           <Col {...twoInCol}>
             <Form.Item label="Title">
-              <Input maxLength={30} />
+              <Input maxLength={30} value={id3.title} />
             </Form.Item>
           </Col>
           <Col {...twoInCol}>
             <Form.Item label="Artist">
-              <Input maxLength={30} />
+              <Input maxLength={30} value={id3.artist} />
             </Form.Item>
           </Col>
         </Row>
         <Row gutter={5} justify="space-around">
           <Col {...twoInCol}>
             <Form.Item label="Album">
-              <Input maxLength={30} />
+              <Input maxLength={30} value={id3.album} />
             </Form.Item>
           </Col>
           <Col {...twoInCol}>
             <Form.Item label="Year">
-              <InputNumber min={0} precision={0} maxLength={4} />
+              <InputNumber min={0} precision={0} maxLength={4} value={id3.year} />
             </Form.Item>
           </Col>
         </Row>
         <Row gutter={5} justify="space-around">
           <Col span={21} offset={1}>
             <Form.Item label="Comment">
-              <TextArea maxLength={30} />
+              <TextArea maxLength={30} value={id3.comment} />
             </Form.Item>
           </Col>
         </Row>
         <Row gutter={5} justify="space-around">
           <Col {...twoInCol}>
             <Form.Item label="Genre">
-              <AutoComplete placeholder="Genre">
+              <AutoComplete placeholder="Genre" value={id3.genre}>
                 {genres.map((genre: Genre) => (
                   <Option key={genre.num}>{genre.genre}</Option>
                 ))}
