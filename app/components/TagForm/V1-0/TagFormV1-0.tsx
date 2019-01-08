3fdedcbf8dad7f249b4fafcde0b2a99d17315a92
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-01-08 09:58:50 +0000

    chore: remove saving state

--- app/components/TagForm/V1-0/TagFormV1-0.tsx
+++ app/components/TagForm/V1-0/TagFormV1-0.tsx
@@ -29,7 +29,7 @@ const oneInCol = {
 export class TagFormV10 extends Component<ITagFormV10Props, ITagFormV10State> {
   constructor(props: ITagFormV10Props) {
     super(props);
-    this.state = { id3: new ID3V10(), saving: false };
+    this.state = { id3: new ID3V10() };
     this.saveFile = this.saveFile.bind(this);
     this.deleteTag = this.deleteTag.bind(this);
     this.onTextInputChange = this.onTextInputChange.bind(this);
@@ -48,20 +48,20 @@ export class TagFormV10 extends Component<ITagFormV10Props, ITagFormV10State> {
   }
 
   public render(): JSX.Element {
-    const { id3, saving } = this.state;
+    const { id3 } = this.state;
     return (
       <Form>
         <Row gutter={5} justify="center">
           <Col {...oneInCol}>
             <Form.Item>
-              <Button type="primary" htmlType="button" disabled={saving} onClick={this.saveFile}>
+              <Button type="primary" htmlType="button" onClick={this.saveFile}>
                 Save tag
               </Button>
             </Form.Item>
           </Col>
           <Col {...oneInCol}>
             <Form.Item>
-              <Button type="danger" htmlType="button" disabled={saving} onClick={this.deleteTag}>
+              <Button type="danger" htmlType="button" onClick={this.deleteTag}>
                 Delete tag
               </Button>
             </Form.Item>
@@ -145,7 +145,6 @@ export class TagFormV10 extends Component<ITagFormV10Props, ITagFormV10State> {
   }
 
   public async constructID3(props: ITagFormV10Props = this.props): Promise<ID3V10> {
-    this.setSaving();
     const { selectedFile } = props;
     const dataView: DataView = await BlobUtil.blobToDataView(selectedFile.originFileObj);
     let id3: ID3V10;
@@ -154,19 +153,16 @@ export class TagFormV10 extends Component<ITagFormV10Props, ITagFormV10State> {
     } else {
       id3 = new ID3V10();
     }
-    this.setSaving(false);
     return id3;
   }
 
   private async saveFile(): Promise<void> {
-    this.setSaving();
     const { selectedFile } = this.props;
     const { id3 } = this.state;
     const electronFile: any = selectedFile.originFileObj;
     const dataView: DataView = Id3Writer.convertID3V10ToDataView(id3);
     await fsUtil.truncate(electronFile.path, 128);
     await fsUtil.writeToFile(electronFile.path, dataView);
-    this.setSaving(false);
     TagFormV10.openNotification('Tag has been saved');
   }
 
@@ -209,9 +205,5 @@ export class TagFormV10 extends Component<ITagFormV10Props, ITagFormV10State> {
     const { id3 } = this.state;
     id3.genre = Id3Reader.convertIndexToGenre(Number.parseInt(value, 10));
     this.setState({ id3 });
-  }
-
-  private setSaving(saving: boolean = true): void {
-    this.setState({ saving });
   }
 }
