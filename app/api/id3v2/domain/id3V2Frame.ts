3530a3ca46eb0cd43480350153ddc35c5bdfc7c8
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-01-14 08:09:40 +0000

    Feat/id3v20 writer/flags (#114)
    * feat: add common flags class for ID3V2.0
    
    * fix reference to frame flag

--- app/api/id3v2/domain/id3V2Frame.ts
+++ app/api/id3v2/domain/id3V2Frame.ts
@@ -1,11 +1,12 @@
 import { FrameID } from './frameID';
+import Id3v2Flags from './id3v2Flags';
 
 export default class {
   public frameID: FrameID | string;
   public size: number;
-  public flags: string;
+  public flags: Id3v2Flags;
 
-  constructor(frameID: FrameID | string, size: number, flags: string) {
+  constructor(frameID: FrameID | string, size: number, flags: Id3v2Flags) {
     this.frameID = frameID;
     this.size = size;
     this.flags = flags;
