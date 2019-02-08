bee51fedd4c38781b0c364324efbd14d0b094797
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-02-08 20:26:12 +0000

    add helper method to read data

--- app/api/id3v2/domain/2.3/id3v2Frame.ts
+++ app/api/id3v2/domain/2.3/id3v2Frame.ts
@@ -1,14 +1,15 @@
 import ID3V2FrameFlags from './id3v2FrameFlags';
+import APICFrame from './apicFrame';
 
 export default class {
   public frameID: string;
   public size: number;
   public flags: ID3V2FrameFlags;
-  public data: string;
+  public data: string | APICFrame;
 
-  constructor(frameID: string, flags: ID3V2FrameFlags, data: string, size?: number) {
+  constructor(frameID: string, flags: ID3V2FrameFlags, data: string, size: number) {
     this.frameID = frameID;
-    this.size = size ? size : data.length;
+    this.size = size;
     this.flags = flags;
     this.data = data;
   }
