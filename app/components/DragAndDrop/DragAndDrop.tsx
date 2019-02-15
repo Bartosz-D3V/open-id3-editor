c71e6a63e10aae74db0c705ce8493a0f592e22c7
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-02-15 09:28:10 +0000

    feat: validate file format on file upload (#153)

--- app/components/DragAndDrop/DragAndDrop.tsx
+++ app/components/DragAndDrop/DragAndDrop.tsx
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router';
 import styled from 'styled-components';
 import { Upload, Icon, message } from 'antd';
 import { UploadChangeParam } from 'antd/lib/upload';
-import { UploadFile } from 'antd/lib/upload/interface';
+import { UploadFile, RcFile } from 'antd/lib/upload/interface';
 import { IDragAndDropProps } from './IDragAndDropProps';
 
 const Dragger = Upload.Dragger;
@@ -15,6 +15,37 @@ class DragAndDrop extends Component<IDragAndDropProps> {
   constructor(props: IDragAndDropProps) {
     super(props);
     this.onDrop = this.onDrop.bind(this);
+    this.validateFormat = this.validateFormat.bind(this);
+  }
+
+  private static onlyMP3s = (files: Array<File>): boolean =>
+    files.filter(v => v.type === 'audio/mp3').length === files.length;
+
+  public render(): JSX.Element {
+    return (
+      <DraggerWrapper>
+        <Dragger
+          multiple
+          showUploadList={false}
+          onChange={this.onDrop}
+          beforeUpload={this.validateFormat}
+        >
+          <p className="ant-upload-drag-icon">
+            <Icon type="inbox" />
+          </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        </Dragger>
+      </DraggerWrapper>
+    );
+  }
+
+  public validateFormat(file: RcFile, files: Array<RcFile>): any {
+    const onlyMP3s = DragAndDrop.onlyMP3s(files);
+    if (!onlyMP3s) {
+      message.error('Please provide MP3 file(s).');
+      files.length = 0;
+    }
+    return onlyMP3s;
   }
 
   public onDrop(info: UploadChangeParam): void {
@@ -26,19 +57,6 @@ class DragAndDrop extends Component<IDragAndDropProps> {
     } else if (status === 'error') {
       message.error(`${info.file.name} file upload failed.`);
     }
-  }
-
-  public render(): JSX.Element {
-    return (
-      <DraggerWrapper>
-        <Dragger multiple showUploadList={false} onChange={this.onDrop}>
-          <p className="ant-upload-drag-icon">
-            <Icon type="inbox" />
-          </p>
-          <p className="ant-upload-text">Click or drag file to this area to upload</p>
-        </Dragger>
-      </DraggerWrapper>
-    );
   }
 }
 export default withRouter(DragAndDrop);
